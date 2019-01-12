70018cef0dc3009fbd9eedd8025a9fa985271992
Marvin Wortman <wortman.marvin@gmail.com>
2019-01-12 20:25:12 +0100

    some fiexes

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class ScoreScript : MonoBehaviour {
 
     public int score = 0;
+
+    void Start()
+    {
+        GameObject _hp = GameObject.Find("HP");
+        GameObject _score = GameObject.Find("Score");
+
+        // string _hpval = _hp.GetComponent<Text>();
+    }
 }
