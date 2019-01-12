75aba34b7bc4f503bbe7f4f643f340522ab021ae
Marvin Wortman <wortman.marvin@gmail.com>
2019-01-12 15:56:00 +0100

    restore hp

---
+++ Assets/Scripts/HPUpScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPUpScript : MonoBehaviour {
+
+    public int restore = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        string colliderName = otherCollider.gameObject.name;
+        if (colliderName == "Hero")
+        {
+            HealthScript life = otherCollider.GetComponent<HealthScript>();
+            life.hp++;
+            Destroy(this.gameObject);
+        }
+    }
+}
